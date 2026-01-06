b3d1b35880060b56681106718c06116b5cb8feab
Timo Mueller <work@mtimo.de>
2026-01-06 23:03:50 +0100

    initial commit

---
+++ create_label.py
@@ -0,0 +1,48 @@
+import requests
+import os.path
+from dotenv import load_dotenv
+load_dotenv()
+
+
+requests.packages.urllib3.disable_warnings()
+AUTH_CODE = os.getenv('auth_code')
+BASE_URL = os.getenv('base_url')
+
+def write_label(label_content, item_path):
+    item_id = item_path.split('/')[2]
+    with (open(f"./created_tags/{item_id}", 'w+')) as label_file:
+        label_file.write(label_content)
+
+def create_label(full_name, item_path):
+    if item_already_handled(item_path):
+        return ""
+    final_label_content = ""
+    with open("./base_tag.lemd") as labelFile:
+        labelFileContent = labelFile.read()
+        item_name = input(f"Name on label for \"{full_name}\": ")
+        final_label_content = labelFileContent.replace("ITEMURL", item_path.replace("/","\\/")).replace("TEXTPLACEHOLDER", item_name)
+    return final_label_content
+
+def item_already_handled(item_path):
+    item_id = item_path.split('/')[2]
+    return os.path.isfile(f"./created_tags/{item_id}")
+    
+headers = {"Authorization": AUTH_CODE}
+resp = requests.get(f'{BASE_URL}/api/v1/items/export',verify=False, headers=headers)
+
+csv_file_lines = resp.text.split("\n")[1:]
+
+for line in csv_file_lines:
+    try:
+        item_path = line.split(',')[5]
+        full_name = line.split(',')[6]
+        final_item_label = create_label(full_name, item_path)        
+    except Exception as e:
+        # Should only trigger for empty lines. TODO better error handling.
+        pass
+    write_label(final_item_label, item_path)
+
+
+# handle csv from resp
+pass
+# create for all which are not generated yet
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from homebox-epson-labelcreator!")
+
+
+if __name__ == "__main__":
+    main()
