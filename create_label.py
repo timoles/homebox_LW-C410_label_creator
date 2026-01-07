cfb7d278167a84998690f8090f9d8864c15a7fcd
Timo Mueller <work@mtimo.de>
2026-01-07 11:08:04 +0100

    minor refactoring

--- create_label.py
+++ create_label.py
@@ -1,6 +1,8 @@
 import requests
 import os.path
 from dotenv import load_dotenv
+import json
+import copy
 
 load_dotenv()
 
@@ -8,25 +10,30 @@ requests.packages.urllib3.disable_warnings()
 AUTH_CODE = os.getenv('auth_code')
 BASE_URL = os.getenv('base_url')
 
-def write_label(label_content, item_path):
+def get_item_id(item_path):
     item_id = item_path.split('/')[2]
-    with (open(f"./created_tags/{item_id}.lemd", 'w+')) as label_file:
+    return item_id
+
+def write_label(label_content, item_path):
+    item_id = get_item_id(item_path)
+    with (open(f"./created_tags/{item_id}.lemd", 'w')) as label_file:
         label_file.write(label_content)
 
-def create_label(full_name, item_path):
-    if item_already_handled(item_path):
-        return ""
-    final_label_content = ""
-    with open("./base_tag.lemd") as labelFile:
-        labelFileContent = labelFile.read()
+def get_item_name(full_name):
         item_name = input(f"Name on label for \"{full_name}\": ")
         if item_name == "":
             item_name = full_name
+        return item_name
+
+def create_label(item_name, item_path):
+    final_label_content = ""
+    with open("./base_tag.lemd") as labelFile:
+        labelFileContent = labelFile.read()
         final_label_content = labelFileContent.replace("ITEMURL", item_path.replace("/","\\/")).replace("TEXTPLACEHOLDER", item_name)
     return final_label_content
 
 def item_already_handled(item_path):
-    item_id = item_path.split('/')[2]
+    item_id = get_item_id(item_path)
     return os.path.isfile(f"./created_tags/{item_id}.lemd")
 
 headers = {"Authorization": AUTH_CODE} # TODO real login or check how to set API key. Currently taken from browser cookie
@@ -37,13 +44,16 @@ csv_file_lines = resp.text.split("\n")[1:]
 for line in csv_file_lines:
     try:
         item_path = line.split(',')[5]
-        full_name = line.split(',')[6]
-        final_item_label = create_label(full_name, item_path)        
+        full_name = line.split(',')[6]  
     except Exception as e:
         # Should only trigger for empty lines. TODO better error handling.
-        pass
-    write_label(final_item_label, item_path)
+        continue
 
+    if not item_already_handled(item_path):
+        print(item_path)
+        item_name = get_item_name(full_name)
+        final_item_label = create_label(item_name, item_path)
+        write_label(final_item_label, item_path)
 
 # handle csv from resp
 pass
